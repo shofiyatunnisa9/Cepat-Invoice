1104b3ec1cbd81915b03ba823b3d7af3ed656d00
mamsky <pasteprosmana189@gmail.com>
2025-05-27 14:19:48 +0700

    features dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
 
 function App() {
-  return (
-    <>
-      <h1>helloword</h1>
-    </>
-  );
+  return <RouterProvider router={router} />
 }
 
 export default App;
