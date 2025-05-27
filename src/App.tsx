f40996c7768024820e0ec71bec49d54cc73412ea
shofiyatunnisa9 <shofiyatunnisa939@gmail.com>
2025-05-27 14:41:09 +0700

    install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import { router } from "./routes/router";
 
 function App() {
 
-=======
   return <RouterProvider router={router} />
->>>>>>> 1104b3ec1cbd81915b03ba823b3d7af3ed656d00
 }
 
 export default App;
