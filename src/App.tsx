c198177b2d0d4d77636e7fabc8620f503d45d515
shofiyatunnisa9 <shofiyatunnisa939@gmail.com>
2025-05-27 15:06:30 +0700

    update : login-form

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
 
 function App() {
-
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
 export default App;
