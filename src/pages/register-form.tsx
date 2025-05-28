88bf38732014441502566f30c62f045c19122ecb
shofiyatunnisa9 <shofiyatunnisa939@gmail.com>
2025-05-28 09:06:03 +0700

    register-form

---
+++ src/pages/register-form.tsx
@@ -0,0 +1,12 @@
+import Register from "@/features/auth/Register";
+import React from "react";
+
+function registerForm() {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+}
+
+export default registerForm;
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,14 @@
 import DashboardPage from "@/pages/dashboard";
+import registerForm from "@/pages/register-form";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
   {
-    path:'/',
-    Component: DashboardPage
-  }
-])
+    path: "/",
+    Component: DashboardPage,
+  },
+  {
+    path: "/register",
+    Component: registerForm,
+  },
+]);
