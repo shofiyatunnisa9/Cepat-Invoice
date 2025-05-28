4432e66a6ca6a76b9fcbdc40f347cee1fd2b515b
shofiyatunnisa9 <shofiyatunnisa939@gmail.com>
2025-05-28 10:04:21 +0700

    update register

--- src/features/auth/Register.tsx
+++ src/features/auth/Register.tsx
@@ -1,10 +1,27 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-
+import { authSchema, type authSchemaDTO } from "@/lib/schemas/schemaRegister";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<authSchemaDTO>({
+    mode: "onChange",
+    resolver: zodResolver(authSchema),
+  });
+
+  const submit = (data: authSchemaDTO) => {
+    console.log(data);
+  };
   return (
     <div className="w-screen h-screen">
-      <form className="flex justify-center items-center h-full  text-center ">
+      <form
+        className="flex justify-center items-center h-full  text-center "
+        onSubmit={handleSubmit(submit)}
+      >
         <div className="bg-blue-500 p-10 pt-2 rounded-xl border w-sm">
           <h1 className="text-black font-bold my-5 text-3xl">Register Pages</h1>
           <div className="my-2 text-left">
@@ -16,7 +33,9 @@ function Register() {
               placeholder="Email"
               type="email "
               id="email"
+              {...register("email")}
             />
+            {errors.email && <p>{errors.email.message}</p>}
           </div>
           <div className="my-5 text-left">
             <label htmlFor="password" className="">
@@ -27,7 +46,9 @@ function Register() {
               type="password"
               id="password"
               placeholder="Password"
+              {...register("password")}
             />
+            {errors.password && <p>{errors.password.message}</p>}
           </div>
           <Button className="bg-blue-950 cursor-pointer px-15" type="submit">
             Register
